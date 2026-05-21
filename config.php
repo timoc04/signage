d3be01bec194840e402636f3daf74d62fa97634a
Timo Claessens <timoclaessens04@gmail.com>
2026-05-21 07:58:16 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 
-const ADMIN_PASSWORD = 'WACHTWOORD_HIER_INVULLEN';
+const ADMIN_PASSWORD = 'WACHTWOORD123';
 
 const MEDIA_DIR = __DIR__ . '/media';
 const MEDIA_URL = 'media';
 
+const SETTINGS_FILE = __DIR__ . '/settings.json';
+
 const ALLOWED_EXTENSIONS = ['jpg', 'jpeg', 'png', 'webp', 'mp4'];
 
-/*
- * Session timeout in seconds.
- * 1800 seconds = 30 minutes.
- */
+const IMAGE_DEFAULT_DURATION = 10;
+
 const SESSION_TIMEOUT = 1800;
